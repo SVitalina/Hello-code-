427377e7cfa05c27e9ee8ff42da2c4a4fd19a6bd
Oleksii <zankexd@icloud.com>
2022-06-29 11:12:18 +0200

    add exam 005 if esle

---
+++ Exam005_if_Esle/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write (" Ввeдите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username == "Виталина")
+{
+    Console.WriteLine("Ура, это же Виталина"); 
+}
+else 
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
