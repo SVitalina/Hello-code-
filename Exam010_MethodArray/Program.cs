3ca772fdc44e329c9b004b1702228dc658d2b12a
Oleksii <zankexd@icloud.com>
2022-06-30 12:16:56 +0200

    Exam010 Method Array

---
+++ Exam010_MethodArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = { 16, 27, 73, 4, 75, 6, 37, 80 };
+
+int n = array.Length;
+int find = 37;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+    }
+    index++;
+}
