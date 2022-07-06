66a5376ad22d4baab9d6a0a8a6093848caa915d3
Oleksii <zankexd@icloud.com>
2022-07-06 11:51:40 +0200

    Exam 013 Task1

---
+++ Exam013_Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Работа с текстом 
+// В тексте нужно заменить все пробелы черточками
+// маленький буквы к заменит большими К
+// а большие С заменить маленькими с 
+
+string text = "– Я думаю, – сказал Князь, улыбаясь, – что,"
+            + "ежели бы вас послали вместо нашего милого Винценгероде, " 
+            + "вы бы взяли приступом согласие прусского короля." 
+            +" Вы так красноречивы. Вы дадите мне чаю?";
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if(text[i]== oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+string newText = Replace(text,' ', '|');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText,'к', 'К');
